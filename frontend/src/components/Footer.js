d369d9437898f9613fac1d58a3564db39c18f7b6
Ömer Furkan İşleyen <omergumushane@hotmail.com>
2022-04-27 20:24:37 +0300

    Update footer for github link

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,16 +1,15 @@
-import { Link } from "react-router-dom";
-
 const Footer = () => {
   return (
     <footer className="mt-auto text-white-50 text-center">
       <p>
-        <Link
-          to="https://github.com/OmerFI"
+        <a
+          href="https://github.com/OmerFI"
           target="_blank"
           className="text-white me-1"
+          rel="noreferrer"
         >
           ÖmerFİ
-        </Link>
+        </a>
         tarafından hazırlanmıştır.
       </p>
     </footer>
