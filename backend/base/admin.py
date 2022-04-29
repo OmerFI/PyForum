c2e93b6f33ee706dd1fdc2ba1fa1b1dca7e164f3
Ömer Furkan İşleyen <omergumushane@hotmail.com>
2022-04-29 15:16:51 +0300

    Customize admin page

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -3,9 +3,36 @@ from .models import User, Profile, Category, Post, Comment, Reply
 
 # Register your models here.
 
-admin.site.register(User)
-admin.site.register(Profile)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("content", "author", "created_at")
+    list_filter = ("created_at",)
+    ordering = ("-created_at",)
+    search_fields = ("content", "author__username")
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "created_at")
+    list_filter = ("created_at",)
+    ordering = ("-created_at",)
+    search_fields = ("title", "author__username")
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "first_name", "last_name", "image")
+    ordering = ("user__username",)
+    search_fields = ("user__username",)
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "first_name", "last_name")
+    ordering = ("username",)
+    search_fields = ("username", "email", "first_name", "last_name")
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Profile, ProfileAdmin)
 admin.site.register(Category)
-admin.site.register(Post)
-admin.site.register(Comment)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
 admin.site.register(Reply)
