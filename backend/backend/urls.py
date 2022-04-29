3187f633da5411c94080ab36784d92ebb05dfe39
Ömer Furkan İşleyen <omergumushane@hotmail.com>
2022-04-29 15:54:47 +0300

    Uncomment out path specifications for react

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -22,8 +22,8 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
-    # path('', TemplateView.as_view(template_name="index.html")),
-    # re_path(r'^(?P<path>.*)/$', TemplateView.as_view(template_name="index.html")),
+    path('', TemplateView.as_view(template_name="index.html")),
+    re_path(r'^(?P<path>.*)/$', TemplateView.as_view(template_name="index.html")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
