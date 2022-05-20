a13eb26a17f462299008b1176f5596c97562408a
Ömer Furkan İşleyen <54645034+OmerFI@users.noreply.github.com>
2022-05-20 12:43:05 +0300

    Add more list filters for the admin page

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -21,12 +21,19 @@ class PostAdmin(admin.ModelAdmin):
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ("user", "first_name", "last_name", "image")
     ordering = ("user__username",)
+    list_filter = (
+        "user__date_joined",
+        "user__is_active",
+        "user__is_staff",
+        "user__is_superuser",
+    )
     search_fields = ("user__username",)
 
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ("username", "email", "first_name", "last_name")
+    list_display = ("username", "email", "first_name", "last_name", "date_joined")
     ordering = ("username",)
+    list_filter = ("date_joined", "is_active", "is_staff", "is_superuser")
     search_fields = ("username", "email", "first_name", "last_name")
 
 
