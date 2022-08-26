0d3f87a1d28a872362acad2e166a2bc698ce2876
Ömer Furkan İşleyen <54645034+OmerFI@users.noreply.github.com>
2022-08-27 01:13:07 +0300

    Remove unnecessary if statement

--- frontend/src/Settings.js
+++ frontend/src/Settings.js
@@ -1,9 +1,6 @@
 const DEBUG = process.env.REACT_APP_DEBUG || true;
 
-let BASE_URL = process.env.REACT_APP_BASE_URL || "";
-if (DEBUG) {
-  BASE_URL = "http://127.0.0.1:8000";
-}
+let BASE_URL = process.env.REACT_APP_BASE_URL || "http://127.0.0.1:8000";
 
 const Settings = () => {
   return { DEBUG, BASE_URL };
