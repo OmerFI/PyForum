ad13cf0283e0911757372a1d8f399debcfe80683
Ömer Furkan İşleyen <54645034+OmerFI@users.noreply.github.com>
2022-08-27 01:06:35 +0300

    Use environment variables for settings

--- frontend/src/Settings.js
+++ frontend/src/Settings.js
@@ -1,6 +1,6 @@
-const DEBUG = false;
+const DEBUG = process.env.REACT_APP_DEBUG || true;
 
-let BASE_URL = "";
+let BASE_URL = process.env.REACT_APP_BASE_URL || "";
 if (DEBUG) {
   BASE_URL = "http://127.0.0.1:8000";
 }
