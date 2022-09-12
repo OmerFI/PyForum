87d0775bedc8900e2d2c99622f14ef5760da893e
Ömer Furkan İşleyen <54645034+OmerFI@users.noreply.github.com>
2022-09-13 00:03:55 +0300

    Use Sentry for exception monitoring

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+
+if (process.env.REACT_APP_SENTRY_DSN) {
+  Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY_DSN,
+    integrations: [new BrowserTracing()],
+
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+  });
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
